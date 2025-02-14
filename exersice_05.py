aa3e36bfa5840de6c38ac2146716189512c7b0bc
kentzy1313 <sebastiantyron_bsit@plmun.edu.ph>
2025-02-14 13:34:57 +0800

    Submitting Python Week 02 - Session 01 - Exercise 05

--- exersice_05.py
+++ exersice_05.py
@@ -17,6 +17,7 @@ def main():
     print(f"User number - 5  = {subtraction:.2f}")
     print(f"User number * 2  = {multiplication:.2f}")
     print(f"User number / 3  = {division:.2f}")
+
     print("\nMath Library Functions:")
     print(f"Square root of {user_number:.2f} is: {sqrt_value}")
     print(f"Sine of {user_number:.2f} degrees is: {sine_value:.4f}") # Sine formatted to 4 decimal places
